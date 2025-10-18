63f31cc51a72968a892ec34db2c08f88ebee14eb
AFFAN LATIF <affanlatifcs@gmail.com>
2025-10-18 12:42:26 +0530

    Create Counter.sol

---
+++ Counter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+    // State variable to store the counter value
+    int public count; // using int so we can go below 0 if decremented
+
+    // Function to increment the counter
+    function increment() public {
+        count += 1;
+    }
+
+    // Function to decrement the counter
+    function decrement() public {
+        count -= 1;
+    }
+
+    // Function to get the current counter value (optional, since 'count' is public)
+    function getCount() public view returns (int) {
+        return count;
+    }
+}

fd7478d9b9e758f78a52d82f0886afb757abaedc
AFFAN LATIF <affanlatifcs@gmail.com>
2025-10-18 12:49:26 +0530

    Add ReputationTracker contract for user achievements

---
+++ Achivement.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ReputationTracker {
+    // Struct to store user details
+    struct User {
+        uint points;
+        uint achievements;
+        string[] badges;
+    }
+
+    // Mapping to store user data by their address
+    mapping(address => User) public users;
+
+    // Event logs for actions
+    event PointsAdded(address indexed user, uint newPoints);
+    event AchievementUnlocked(address indexed user, string badgeName);
+
+    // Function to add points to a user's reputation
+    function addPoints(uint _points) public {
+        users[msg.sender].points += _points;
+        emit PointsAdded(msg.sender, users[msg.sender].points);
+    }
+
+    // Function to add an achievement/badge
+    function unlockBadge(string memory _badgeName) public {
+        users[msg.sender].achievements += 1;
+        users[msg.sender].badges.push(_badgeName);
+        emit AchievementUnlocked(msg.sender, _badgeName);
+    }
+
+    // Function to view user details
+    function getUser(address _user) public view returns (uint, uint, string[] memory) {
+        User storage user = users[_user];
+        return (user.points, user.achievements, user.badges);
+    }
+}
